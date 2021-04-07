09950997e793b00aa9afce961f531bb0c88241c8
John <katway@163.com>
2021-04-07 19:44:18 +0800

    更新Gradle为4.1.3

--- app/src/main/java/com/kongqw/kqwrockerdemo/MainActivity.java
+++ app/src/main/java/com/kongqw/kqwrockerdemo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.kongqw.kqwrockerdemo;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.kongqw.rockerlibrary.view.RockerView;
 
